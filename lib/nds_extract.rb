05f83e5c1f822200af319e494933e04af6010f93
Edward Castillo <castilloedwarda@gmail.com>
2019-11-15 04:17:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -53,10 +53,10 @@ def movies_with_director_key(name, movies_collection)
   i = 0
   while i < movies_collection.length do
     movie_data = movies_collection[i]
-    result << movie_with_director_name(name, movie_data)
+    new_array << movie_with_director_name(name, movie_data)
     i += 1
   end
- result
+ new_array
 end
 
 
